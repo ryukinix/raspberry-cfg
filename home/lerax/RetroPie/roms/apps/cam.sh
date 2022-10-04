9d5afb8c9472938104921520ac39079d27ae87c2
Manoel Vilela <manoel_vilela@engineer.com>
2022-10-04 00:13:40 -0300

    [cam] Add comment about my password

--- home/lerax/RetroPie/roms/apps/cam.sh
+++ home/lerax/RetroPie/roms/apps/cam.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 base_path=/home/lerax/RetroPie/roms/apps
 source $base_path/exit_joystick.sh.lib
+# my password in the cam_url.txt file
 cam_url="$(cat $base_path/cam_url.txt)"
 exit_joystick&
 mpv "${cam_url}/cam/realmonitor?channel=1&subtype=1" > /dev/null
