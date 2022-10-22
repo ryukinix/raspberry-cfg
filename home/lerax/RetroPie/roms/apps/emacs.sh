c27026b190d1b3d1214dedf8ee124263c9c31e72
Manoel Vilela <manoel_vilela@engineer.com>
2022-10-22 17:48:08 -0300

    [emacs] Improve resilience of emacs app

--- home/lerax/RetroPie/roms/apps/emacs.sh
+++ home/lerax/RetroPie/roms/apps/emacs.sh
@@ -1,2 +1,26 @@
 #!/bin/bash
-emacsclient -nw -a "emacs"
+emacs-open () {
+    emacsclient -nw
+}
+
+emacs-open-tmux () {
+    tmux new-session -A -s emacs -E '/usr/bin/emacsclient -nw'
+}
+
+emacs-restart () {
+    echo "Kill all emacs daemons"
+    systemctl --user stop emacs
+    echo "Start new emacs daemon"
+    systemctl --user start emacs
+}
+
+emacs-not-success () {
+    [[ $? != 0 ]]
+}
+
+main () {
+    emacs-open-tmux
+    emacs-not-success && emacs-restart && emacs-open-tmux
+}
+
+main
